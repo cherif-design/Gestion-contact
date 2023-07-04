a5c3b49888d71d136e2578f97ea4a9235725b975
cherif-design <bakis1011@gmail.com>
2023-07-04 12:10:48 +0000

    Add projet-6.py and Contact db

--- projet-6.py
+++ projet-6.py
@@ -3,9 +3,9 @@ import sqlite3
 conn =sqlite3.connect('Contact.db')
 cur = conn.cursor()
 #Création de la table Contact
-cur.execute(
+"""cur.execute(
     "CREATE TABLE Contact(nom text, prenom text, email text, telephone integer)"
-)
+)"""
 #declaration de la class contact
 class Contact:
     def __init__(self, nom, prenom, email, telephone):
@@ -14,45 +14,48 @@ class Contact:
         self.email = email
         self.telephone = telephone
     def inserer(self):
-        cur.execute(
-            "INSERT INTO Contact(nom, prenom, email, telephone) VALUES(?, ?, ?, ?)",
-            (self.nom, self.prenom, self.email, self.telephone),
-        )
-#definition de la fonction ajouter    
-    def ajouter_contact(nom, prenom, email, telephone):
-        cur.execute(
-            "INSERT INTO Contact(nom, prenom, email, telephone) VALUES(?, ?, ?, ?)",
-            (nom, prenom, email, telephone),
-        )
-#definition de la fonction modifier        
-    def modifier_contact(nouveau_numero, ancien_numero):
-        cur.execute(
-            "UPDATE Contact SET telephone = ? WHERE telephone = ?",
-            (nouveau_numero, ancien_numero),
-        )
-#definition de la fonction supprimer
-    def supprimer_contact(telephone):
-        cur.execute("DELETE FROM Contact WHERE telephone = ?", (telephone,))
+        cur.execute("INSERT INTO Contact(nom, prenom, email, telephone) VALUES(?, ?, ?, ?)",
+                    (self.nom, self.prenom, self.email, self.telephone))
+        conn.commit()
 
-#definition de la fonction affichage liste
-    def afficher_liste_contact():
+    
+    def ajouter_contact(self, nom, prenom, email, telephone):
+        contact = Contact(nom, prenom, email, telephone)
+        contact.inserer()
+
+    
+    def modifier_contact(self, nouveau_numero, ancien_numero):
+        cur.execute("UPDATE Contact SET telephone = ? WHERE telephone = ?", (nouveau_numero, ancien_numero))
+        conn.commit()
+
+    
+    def supprimer_contact(self, telephone):
+        cur.execute("DELETE FROM Contact WHERE telephone = ?", (telephone,))
+        conn.commit()
+
+
+    def afficher_liste_contact(self):
         rows = cur.execute("SELECT * FROM Contact").fetchall()
         for row in rows:
             print(row)
-#definition de la fonction recherche
-    def rechercher_numero_contact(telephone):
-        row = cur.execute(
-            "SELECT * FROM Contact WHERE telephone = ?", (telephone,)).fetchone()
+
+
+    def rechercher_numero_contact(self, telephone):
+        row = cur.execute("SELECT * FROM Contact WHERE telephone = ?", (telephone,)).fetchone()
         print(row)
-#Instance de la class Contact
+        
+#Instance de la classe Contact
 contact = Contact("Aidara", "Cherif", "bakis1011@gmail.com", 775851112)
 contact.inserer()
-Contact.ajouter_contact("Aidara", "Moulaye", "moulaye@gmail.com", 774673332)    
-Contact.ajouter_contact("Aidara", "Fanta", "fantasiré@gmail.com", 784673332)    
-Contact.modifier_contact(707202863, 775851112)
-Contact.supprimer_contact(775851112)
-Contact.afficher_liste_contact()
-Contact.rechercher_numero_contact(774673332)
-#Insertion des données de l'objet dans la base de données
+
+contact = Contact("Aidara", "Moulaye", "moulaye@gmail.com", 774673332)
+contact.ajouter_contact(contact.nom, contact.prenom, contact.email, contact.telephone)
+
+#Méthode de la classe 
+contact.modifier_contact(787202863, contact.telephone)
+contact.supprimer_contact(775851112)
+contact.afficher_liste_contact()
+contact.rechercher_numero_contact(774673332)
+
 cur.execute("INSERT INTO Contact VALUES (?, ?, ?, ?)", (contact.nom, contact.prenom, contact.email, contact.telephone))
 conn.commit()
